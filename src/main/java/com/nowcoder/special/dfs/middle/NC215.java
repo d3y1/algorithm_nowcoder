4c64968a913b8342804bb8dbee7ba99e70eb071e
d3y1 <d3y1_2u@163.com>
2024-04-15 17:59:44 +0800

    :bulb: NC215 将二叉搜索树改为累加树: dfs(递归): 中序遍历(变体)[右中左]

---
+++ src/main/java/com/nowcoder/special/dfs/middle/NC215.java
@@ -0,0 +1,50 @@
+package com.nowcoder.special.dfs.middle;
+
+/**
+ * NC215 将二叉搜索树改为累加树
+ * @author d3y1
+ */
+public class NC215{
+    private int preVal = 0;
+
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 程序入口
+     *
+     * @param root TreeNode类
+     * @return TreeNode类
+     */
+    public TreeNode convertBST (TreeNode root) {
+        inOrder(root);
+
+        return root;
+    }
+
+    /**
+     * 递归: 中序遍历(变体)[右中左]
+     * @param root
+     */
+    private void inOrder(TreeNode root){
+        if(root == null){
+            return;
+        }
+
+        inOrder(root.right);
+
+        root.val += preVal;
+        preVal = root.val;
+
+        inOrder(root.left);
+    }
+
+    private class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
